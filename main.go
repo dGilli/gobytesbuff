5520bc65f9042f7508f1b0e86e8ff8593ddb751e
Dennis Gilli <dennis.gilli@acribis.ch>
2024-05-17 09:14:57 +0200

    add close buffer logic

--- main.go
+++ main.go
@@ -7,15 +7,34 @@ import (
 	"log"
 )
 
+type BytesClosingBuffer struct {
+    *bytes.Buffer
+    io.Closer
+}
+
+func NewBytesClosingBuffer() *BytesClosingBuffer {
+    return &BytesClosingBuffer{
+        Buffer: new(bytes.Buffer),
+    }
+}
+
+func (b *BytesClosingBuffer) Close() error {
+    fmt.Println("closing")
+    return nil
+}
+
 func writeTo(w io.Writer, msg []byte) error {
     _, err := w.Write(msg)
     return err
 }
 
 func main() {
-    buf := new(bytes.Buffer)
+    buf := NewBytesClosingBuffer()
+    defer buf.Close()
+
     if err := writeTo(buf, []byte("Happy newyear")); err != nil {
         log.Fatal(err)
+
     }
 
     fmt.Println(buf.String())
